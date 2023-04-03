ee1405a6f6378132ef7e6d5b147824925ba08d27
Blaze <candlepeter56@gmail.com>
2023-04-04 00:26:09 +0100

    corrected file folder

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./src/index.html"),
-        watchlist: resolve(__dirname, "./src/watchlist/watchlist.html"),
+        main: resolve(__dirname, "./index.html"),
+        watchlist: resolve(__dirname, "./watchlist/watchlist.html"),
       },
     },
   },
